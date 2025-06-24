c1a12e88b6af8bba58eddbf661e3955773d293ac
amandamachado1 <145574744+amandamachado1@users.noreply.github.com>
2025-06-24 08:41:05 -0300

    AA

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-11-09T00:00:00");
-const tempoObjetivo2 = new Date("2025-11-30T00:00:00");
+const tempoObjetivo2 = new Date("2025-11-29T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-31T00:00:00");
 const tempoObjetivo4 = new Date("2025-11-17T00:00:00");
 
