74bceeae7c6598375de16f7228e871b7329c63db
amandamachado1 <145574744+amandamachado1@users.noreply.github.com>
2025-06-24 08:08:53 -0300

    aa

--- main.js
+++ main.js
@@ -16,6 +16,18 @@ for (let i=0; i<botoes.length; i++){
 }
  const contadores = document. querySelectorAll(".contador");
  const tempoObjetivo1 = new Dater ("2025-10-16T00:00:00");
+ contadores[0].textContent = calculaTempo(tempoObjetivo);
+  function calculaTempo(tempoObjetivo){
  let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo-tempoAtual;
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
 
-  contadores[0]. textContent = tempoObjetivo - tempoAtual;
+  }
+
+
+
+
+
